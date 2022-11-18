2f5616404a3e67c89273ddba62924d1eb348470c
Marcos Vinicius <marcosviniciusmoraescosta01@gmail.com>
2022-11-17 20:19:52 -0500

    agora devem faltar apenas algusn ajustes...

--- JogodaMemria/app/src/main/java/br/com/pontes/jogodamemria/MainActivity.java
+++ JogodaMemria/app/src/main/java/br/com/pontes/jogodamemria/MainActivity.java
@@ -1,11 +1,18 @@
 package br.com.pontes.jogodamemria;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.text.InputType;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,44 +24,77 @@ public class MainActivity extends AppCompatActivity {
 
     private Controle controle = new Controle();
     private ImageButton[] figurinhas = new ImageButton[24];
+    private List<ImageButton> botoesNaoAchados = new ArrayList<ImageButton>();
     private Carta[] botoes = new Carta[24];
     private List<Integer> cartasMostradas = new ArrayList<Integer>();
-    TextView tv_jogador1;
-    TextView tv_jogador2;
-    TextView tv_pts1;
-    TextView tv_pts2;
-    int jogador = 0;
+    private TextView tv_jogador1;
+    private TextView tv_jogador2;
+    private TextView tv_pts1;
+    private TextView tv_pts2;
+    //private TextView tv_jogadorAtual;
+    private int jogador = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.concatenar();
-        controle.popularlista();
-        this.definirCartaBotoes();
-        this.clicarBotao();
+        //controle.popularlista();
+        //this.definirCartaBotoes();
+        this.virarCartas();
     }
 
-    public void clicarBotao() {
-       virarCartas();
-
-        //desvirarCarta();
-
-    }
     public void desvirarCarta(){
         figurinhas[cartasMostradas.get(0)].setImageResource(botoes[cartasMostradas.get(0)].getFace1());
         figurinhas[cartasMostradas.get(1)].setImageResource(botoes[cartasMostradas.get(1)].getFace1());
+        ativaBotoesNaoAchados();
+        Toast toast = Toast.makeText(getApplicationContext(),"Vez de "+ (jogador%2==1 ? tv_jogador2.getText():tv_jogador1.getText()), Toast.LENGTH_LONG); toast.show();
         cartasMostradas.clear();
     }
 
     public boolean saoIguais(Carta c1, Carta c2, int j){
-        if(c1.getFace2() == c2 .getFace2()){
+        //compara se são iguais
+        if((c1.getFace2() == c2 .getFace2()) ){
+            Toast toast = Toast.makeText(getApplicationContext(),"Vez de "+ (jogador%2==1 ? tv_jogador2.getText():tv_jogador1.getText()), Toast.LENGTH_LONG); toast.show();
+            botoesNaoAchados.remove(figurinhas[cartasMostradas.get(0)]);
+            botoesNaoAchados.remove(figurinhas[cartasMostradas.get(1)]);
+            ativaBotoesNaoAchados();
             if(j == 1){
+                figurinhas[cartasMostradas.get(0)].setColorFilter(getResources().getColor(R.color.green));
+                figurinhas[cartasMostradas.get(1)].setColorFilter(getResources().getColor(R.color.green));
                 tv_pts1.setText((Integer.parseInt(tv_pts1.getText().toString())+1)+"");
             }else {
                 tv_pts2.setText((Integer.parseInt(tv_pts2.getText().toString())+1)+"");
+                figurinhas[cartasMostradas.get(0)].setColorFilter(getResources().getColor(R.color.yellow));
+                figurinhas[cartasMostradas.get(1)].setColorFilter(getResources().getColor(R.color.yellow));
             }
             cartasMostradas.clear();
+            //teste se já acharam todas as cartas
+            if(botoesNaoAchados.size()==0){
+                //empate
+                if ((Integer.parseInt(tv_pts1.getText().toString())) == (Integer.parseInt(tv_pts2.getText().toString()))){
+                    AlertDialog.Builder empate = new AlertDialog.Builder(this);
+                    empate.setTitle("Empate !");
+                    empate.setMessage("ambos os jogadonhes garanham... ou perderam, depende do ponto de vista");
+                    empate.setPositiveButton("OK", null);
+                    empate.create();
+                    empate.show();
+                }else if((Integer.parseInt(tv_pts1.getText().toString())) > (Integer.parseInt(tv_pts2.getText().toString()))){
+                    AlertDialog.Builder alertaVenceu = new AlertDialog.Builder(this);
+                    alertaVenceu.setTitle("Vitória !!");
+                    alertaVenceu.setMessage("O jogador " + tv_jogador1.getText() + " venceu!");
+                    alertaVenceu.setPositiveButton("OK", null);
+                    alertaVenceu.create();
+                    alertaVenceu.show();
+                }else{
+                    AlertDialog.Builder alertaVenceu = new AlertDialog.Builder(this);
+                    alertaVenceu.setTitle("Vitória !!");
+                    alertaVenceu.setMessage("O jogador " + tv_jogador2.getText() + " venceu!");
+                    alertaVenceu.setPositiveButton("OK", null);
+                    alertaVenceu.create();
+                    alertaVenceu.show();
+                }
+            }
             return true;
         }
         return false;
@@ -68,10 +108,12 @@ public class MainActivity extends AppCompatActivity {
                 public void onClick(View view) {
                     figurinhas[finalI].setImageResource(botoes[finalI].getFace2());
                     cartasMostradas.add(finalI);
+                    //previne o clique na mesma carta
+                    figurinhas[finalI].setClickable(false);
+                    figurinhas[finalI].setEnabled(false);
                    if(cartasMostradas.size()==2){
                        jogador++;
-                       //Toast toast = Toast.makeText(getApplicationContext(), "Jgador " + jogador%2 +"\n", Toast.LENGTH_LONG); toast.show();
-
+                       desativaBotoes();
                        if(!saoIguais(botoes[cartasMostradas.get(1)], botoes[cartasMostradas.get(0)], jogador%2)){
                            final Handler handler = new Handler(Looper.getMainLooper());
                            handler.postDelayed(new Runnable() {
@@ -79,7 +121,7 @@ public class MainActivity extends AppCompatActivity {
                                public void run() {
                                    desvirarCarta();
                                }
-                           }, 3000);
+                           }, 1500);
                        }
 
                    }
@@ -92,6 +134,7 @@ public class MainActivity extends AppCompatActivity {
         tv_pts1 = findViewById(R.id.tv_pts1);
         tv_jogador2 = findViewById(R.id.tv_jogador2);
         tv_pts2 = findViewById(R.id.tv_pts2);
+        //tv_jogadorAtual = findViewById(R.id.tv_jogadorAtual);
 
         this.figurinhas[0] = findViewById(R.id.figurinha0);
         this.figurinhas[1] = findViewById(R.id.figurinha1);
@@ -117,7 +160,83 @@ public class MainActivity extends AppCompatActivity {
         this.figurinhas[21] = findViewById(R.id.figurinha21);
         this.figurinhas[22] = findViewById(R.id.figurinha22);
         this.figurinhas[23] = findViewById(R.id.figurinha23);
+        this.desativaBotoes();
+        this.inicializaListaBotoes();
+    }
 
+    public void desativaBotoes(){
+        for (int i = 0; i < 24; i++) {
+            this.figurinhas[i].setClickable(false);
+            this.figurinhas[i].setEnabled(false);
+        }
+    }
+
+    public void inicializaListaBotoes(){
+        for (int i = 0; i < 24; i++) {
+            botoesNaoAchados.add(figurinhas[i]);
+        }
+    }
+
+    public void ativaBotoesNaoAchados(){
+        for(int i = 0; i < botoesNaoAchados.size();i++){
+            botoesNaoAchados.get(i).setEnabled(true);
+            botoesNaoAchados.get(i).setClickable(true);
+        }
+    }
+
+    public void ativaBotoes(){
+        for (int i = 0; i < 24; i++) {
+            this.figurinhas[i].setEnabled(true);
+            this.figurinhas[i].setClickable(true);
+
+        }
+    }
+
+   public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        switch (item.getItemId()){
+            case R.id.comecarJogo:
+                this.reinicializar();
+                final EditText editText2 = new EditText(this);
+                AlertDialog.Builder secondPlayer = new AlertDialog.Builder(this);
+                secondPlayer.setMessage("Nome do jogador 2 (amarelo):");
+                //secondPlayer.setTitle("J2");
+                secondPlayer.setView(editText2);
+                secondPlayer.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String jogador2 = editText2.getText().toString();
+                        tv_jogador2.setText(jogador2 + "");
+                    }
+                });
+                secondPlayer.create();
+                secondPlayer.show();
+
+                final EditText editText1 = new EditText(this);
+                AlertDialog.Builder firstPlayer = new AlertDialog.Builder(this);
+                firstPlayer.setMessage("Nome do jogador 1 (Verde):");
+                // firstPlayer.setTitle("J1");
+                firstPlayer.setView(editText1);
+                firstPlayer.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String jogador1 = editText1.getText().toString();
+                        tv_jogador1.setText(jogador1 + "");
+                        //tv_jogadorAtual.setText("Vez de "+ jogador1);
+                        //Toast toast = Toast.makeText(getApplicationContext(), "Vez de " + jogador1 +"\n", Toast.LENGTH_LONG); toast.show();
+
+                    }
+                });
+                firstPlayer.create();
+                firstPlayer.show();
+                //Toast.makeText(getApplicationContext(), "inicializa novo jogo", Toast.LENGTH_LONG).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void definirCartaBotoes(){
@@ -126,6 +245,20 @@ public class MainActivity extends AppCompatActivity {
             figurinhas[i].setImageResource(c.getFace1());
             botoes[i] = c;
             controle.setVetorCartas(c, i);
+        }
+    }
+    public void reinicializar(){
+        this.ativaBotoes();
+        controle.popularlista();
+        definirCartaBotoes();
+        descolorir();
+        tv_pts1.setText("0");
+        tv_pts2.setText("0");
+    }
+
+    private void descolorir() {
+        for (int i = 0; i < 24; i++) {
+            figurinhas[i].setColorFilter(null);
         }
     }
 }
