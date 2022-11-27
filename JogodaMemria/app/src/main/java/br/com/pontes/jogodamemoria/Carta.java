fbb492887d78ff09c0f179eaa86cd603680e5b41
MarcosVini753 <marcosviniciusmoraescosta01@gmail.com>
2022-11-26 22:52:02 -0500

    melhornado as figuras e ajustando os links...

---
+++ JogodaMemria/app/src/main/java/br/com/pontes/jogodamemoria/Carta.java
@@ -0,0 +1,20 @@
+package br.com.pontes.jogodamemoria;
+
+public class Carta {
+    private int face1;
+    private int face2;
+
+
+    Carta(int face2){
+        this.face1 = R.drawable.copa;
+        this.face2 = face2;
+    }
+
+    public int getFace1() {
+        return face1;
+    }
+
+    public int getFace2() {
+        return face2;
+    }
+}
